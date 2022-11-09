c42b110f78e4d5c4b87b4b35d8c0b7e142d326f4
Yuwen Zhang <yuwen01@berkeley.edu>
2022-11-08 17:37:43 -0800

    final initial setup

--- core-modules/skrecovery/client/main.rs
+++ core-modules/skrecovery/client/main.rs
@@ -7,17 +7,17 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait SecretKeyRecoverable {
     // Encrypt the secret key?
-    async fn upload_sk_and_pwd(&self, sk: String, pwd: String);
-    async fn recover_sk(&self, pwd_guess: String) -> String;
+    async fn upload_sk_and_pwd(&self, id: String, sk: String, pwd: String);
+    async fn recover_sk(&self, id: String, pwd_guess: String) -> String;
 }
 
 #[async_trait]
 impl SecretKeyRecoverable for Client {
-    async fn upload_sk_and_pwd(&self, sk: String, pwd: String) {
+    async fn upload_sk_and_pwd(&self, id: String, sk: String, pwd: String) {
         todo!();
     }
 
-    async fn recover_sk(&self, pwd_guess: String) -> String {
+    async fn recover_sk(&self, id: String, pwd_guess: String) -> String {
         todo!();
     }
 }
@@ -25,7 +25,7 @@ impl SecretKeyRecoverable for Client {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let cmd = &args[1];
+    let cmd =args[1]; &args[1];
 
     let node_addrs = ["http://127.0.0.1:50051", "http://127.0.0.1:50052"];
 
@@ -36,11 +36,60 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match &cmd[..]{
         "upload_sk_and_pwd" => {
+            let id: String = match args[2].parse() {
+                Ok(s) => {
+                    s
+                },
+                Err(_) => {
+                    eprintln!("error: user-id not a string");
+                    panic!("");
+                },
+            };
+            let sk: String = match args[3].parse() {
+                Ok(s) => {
+                    s
+                },
+                Err(_) => {
+                    eprintln!("error: sk not a string");
+                    panic!("");
+                },
+            };
+            let pwd: String = match args[4].parse() {
+                Ok(s) => {
+                    s
+                },
+                Err(_) => {
+                    eprintln!("error: pwd not a string");
+                    panic!("");
+                },
+            };
+            println!("Uploading sk {}, pwd {} for user {}", sk, pwd, id);
+            client.upload_sk_and_pwd(String::from(id), sk, pwd).await;
         }
         "recover_sk" => {
+            let id: String = match args[2].parse() {
+                Ok(s) => {
+                    s
+                },
+                Err(_) => {
+                    eprintln!("error: user-id not a string");
+                    panic!("");
+                },
+            };
+            let pwd_guess: String = match args[3].parse() {
+                Ok(s) => {
+                    s
+                },
+                Err(_) => {
+                    eprintln!("error: pwd guess not a string");
+                    panic!("");
+                },
+            };
+            println!("Recovering sk with pwd guess {}, for user {}", pwd_guess, id);
+            client.recover_sk(String::from(id), pwd_guess).await;
         }
 
-        _=> println!("Missing/wrong arguments")
+        _ => println!("Missing/wrong arguments")
     };
     Ok(())
 }
