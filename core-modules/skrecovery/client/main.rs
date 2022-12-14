9f9afbbd2e128f2940bf0d4b89e34a5c16b95c27
Yuwen Zhang <yuwen01@gmail.com>
2022-12-14 15:50:20 -0800

    Added some unit tests for shard, and cleaned up imports

--- core-modules/skrecovery/client/main.rs
+++ core-modules/skrecovery/client/main.rs
@@ -1,18 +1,19 @@
 use std::env;
 use std::str::FromStr;
 
-use dtrust::client::Client;
 use async_trait::async_trait;
-use rand_chacha::ChaCha20Rng;
+use dtrust::client::Client;
 use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
 
-use ark_ff::{Zero};
 use ark_ff::fields::Field;
+use ark_ff::Zero;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 
-use ark_bls12_381::{Fr as F};
+use ark_bls12_381::Fr as F;
 
-#[path = "../util.rs"] mod util;
+#[path = "../util.rs"]
+mod util;
 use util::shard_to_bytes;
 
 #[async_trait]
@@ -24,18 +25,17 @@ pub trait SecretKeyRecoverable {
 }
 
 #[async_trait]
-impl SecretKeyRecoverable for Client
-{
-
-    // TODO I think we have to use 
+impl SecretKeyRecoverable for Client {
+    // TODO I think we have to use
     async fn upload_sk_and_pwd(&self, num_nodes: usize, id: String, sk: String, pwd: String) {
         let rng = &mut ChaCha20Rng::from_entropy();
         let sk_field = F::from_str(&sk).unwrap();
         let mut sk_shards_bytes = shard_to_bytes::<F>(sk_field, num_nodes, rng);
         sk_shards_bytes.push(Vec::new());
-        self.upload_blob(id.to_owned() + "sk.txt", sk_shards_bytes).await;
+        self.upload_blob(id.to_owned() + "sk.txt", sk_shards_bytes)
+            .await;
 
-        let pwd_field = <F>::from_random_bytes(pwd.as_bytes()).unwrap();
+        let pwd_field = F::from_random_bytes(pwd.as_bytes()).unwrap();
         println!("pwd_field: {}", pwd_field);
         let mut pwd_shards_bytes = shard_to_bytes::<F>(pwd_field, num_nodes, rng);
         pwd_shards_bytes.push(Vec::new());
@@ -47,11 +47,11 @@ impl SecretKeyRecoverable for Client
         let mut guess_shards_bytes = shard_to_bytes::<F>(pwd_guess, num_nodes, rng);
         guess_shards_bytes.push(Vec::new());
         self.upload_blob(id + "guess.txt", guess_shards_bytes).await;
-
     }
     async fn aggregate_sk(&self, num_nodes: usize, id: String) -> Vec<u8> {
         let sk_shard_bytes = self.retrieve_blob(id + "recovered_sk.txt").await;
-        let f = sk_shard_bytes[..num_nodes].iter()
+        let f = sk_shard_bytes[..num_nodes]
+            .iter()
             .map(|v| F::deserialize_uncompressed(v.as_slice()).unwrap())
             .fold(F::zero(), |x, y| x + y);
 
@@ -66,7 +66,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let cmd = &args[1];
 
-    let node_addrs = ["http://127.0.0.1:50051", "http://127.0.0.1:50052", "http://127.0.0.1:50053", "http://127.0.0.1:50054"];
+    // Set up for 3 participating servers, and one beaver triple dealing server
+    let node_addrs = [
+        "http://127.0.0.1:50051",
+        "http://127.0.0.1:50052",
+        "http://127.0.0.1:50053",
+        "http://127.0.0.1:50054",
+    ];
 
     let cli_id = "user1";
     let mut client = Client::new(cli_id);
@@ -76,82 +82,84 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let num_nodes = node_addrs.len() - 1;
     client.setup(node_addrs.to_vec());
 
-    match &cmd[..]{
+    match &cmd[..] {
         "upload_sk_and_pwd" => {
             let id: String = match args[2].parse() {
-                Ok(s) => {
-                    s
-                },
+                Ok(s) => s,
                 Err(_) => {
                     eprintln!("error: user-id not a string");
                     panic!("");
-                },
+                }
             };
             let sk: String = match args[3].parse() {
-                Ok(s) => {
-                    s
-                },
+                Ok(s) => s,
                 Err(_) => {
                     eprintln!("error: sk not a string");
                     panic!("");
-                },
+                }
             };
             let pwd: String = match args[4].parse() {
-                Ok(s) => {
-                    s
-                },
+                Ok(s) => s,
                 Err(_) => {
                     eprintln!("error: pwd not a string");
                     panic!("");
-                },
+                }
             };
             println!("Uploading sk {}, pwd {} for user {}", sk, pwd, id);
-            client.upload_sk_and_pwd(num_nodes, String::from(id), sk, pwd).await;
+            client.upload_sk_and_pwd(num_nodes, id, sk, pwd).await;
         }
         "recover_sk" => {
             let id: String = match args[2].parse() {
-                Ok(s) => {
-                    s
-                },
+                Ok(s) => s,
                 Err(_) => {
                     eprintln!("error: user-id not a string");
                     panic!("");
-                },
+                }
             };
             let pwd_guess = match args[3].parse::<String>() {
-                Ok(s) => {
-                    F::from_random_bytes(s.as_bytes()).unwrap()
-                },
+                Ok(s) => F::from_random_bytes(s.as_bytes()).unwrap(),
                 Err(_) => {
                     eprintln!("error: pwd guess not a string");
                     panic!("");
-                },
+                }
             };
             println!("Uploading guess ...");
-            client.upload_pwd_guess(num_nodes, String::from(&id), pwd_guess).await;
+            client
+                .upload_pwd_guess(num_nodes, String::from(&id), pwd_guess)
+                .await;
             println!("Guess uploaded");
 
-            println!("Recovering sk with pwd guess {}, for user {}", pwd_guess, id);
-            
-            let in_files = [String::from(id.to_owned() + "sk.txt"),
-                String::from(id.to_owned() + "pwd.txt"), 
-                String::from(id.to_owned() + "guess.txt")];
+            println!(
+                "Recovering sk with pwd guess {}, for user {}",
+                pwd_guess, id
+            );
 
-            let out_files = [String::from(id.to_owned() + "recovered_sk.txt")];
+            let in_files = [
+                id.to_owned() + "sk.txt",
+                id.to_owned() + "pwd.txt",
+                id.to_owned() + "guess.txt",
+            ];
+
+            let out_files = [id.to_owned() + "recovered_sk.txt"];
 
             client
-                .exec(app_name, "skrecovery", in_files.to_vec(), out_files.to_vec())
+                .exec(
+                    app_name,
+                    "skrecovery",
+                    in_files.to_vec(),
+                    out_files.to_vec(),
+                )
                 .await?;
 
             println!("Aggregating SK on client");
             let s = client.aggregate_sk(num_nodes, id).await;
 
-            let f = F::deserialize_uncompressed(s.as_slice()).unwrap(); 
-            
+            let f = F::deserialize_uncompressed(s.as_slice()).unwrap();
+
             println!("Recovered sk: {}", f);
         }
 
-        _ => println!("Missing/wrong arguments")
+        _ => println!("Missing/wrong arguments"),
     };
     Ok(())
 }
--- core-modules/skrecovery/server/app.rs
+++ core-modules/skrecovery/server/app.rs
@@ -1,24 +1,25 @@
+use dtrust::utils::init_app;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
 use std::io;
 use std::io::prelude::*;
-use dtrust::utils::init_app;
-use rand_chacha::ChaCha20Rng;
-use rand::prelude::*;
 
-// we use this field for all arithmetic 
+// we use this field for all arithmetic
 // TODO: move this field choice into a config somewhere. Also, put the number of nodes in this config
-// Also the file names we use for client/server communication. 
+// Also the file names we use for client/server communication.
 use ark_bls12_381::Fr as F;
-use ark_ff::{One, Field};
-use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, SerializationError};
+use ark_ff::One;
 use ark_ff::UniformRand;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, SerializationError};
 use std::net::TcpStream;
 
-#[path = "../util.rs"] mod util;
+#[path = "../util.rs"]
+mod util;
 use util::shard_to_bytes;
 
-const F_SIZE: usize = 32; // ceil(381 * 12 / 8)
+const F_SIZE: usize = 32;
 
-fn read_beaver_elt(socks: &mut Vec<TcpStream>, server_id: usize) -> Result<F, SerializationError> {
+fn read_beaver_elt(socks: &mut [TcpStream], server_id: usize) -> Result<F, SerializationError> {
     let mut buf = [0u8; F_SIZE];
     socks[server_id].read(&mut buf)?;
     F::deserialize_uncompressed(buf.as_slice())
@@ -33,8 +34,7 @@ fn main() -> io::Result<()> {
     let num_parties = socks.len() - 1;
 
     match &func_name[..] {
-        "skrecovery" =>
-        {
+        "skrecovery" => {
             if rank == beaver_server as u8 {
                 let rng = &mut ChaCha20Rng::from_entropy();
                 let a = F::rand(rng);
@@ -48,39 +48,39 @@ fn main() -> io::Result<()> {
                 for i in 0..socks.len() {
                     if i != beaver_server {
                         // NOTE: only works if beaver_server is the LAST NODE!
-                        socks[i].write(&a_shards[i])?;
-                        socks[i].write(&b_shards[i])?;
+                        socks[i].write_all(&a_shards[i])?;
+                        socks[i].write_all(&b_shards[i])?;
                         socks[i].write(&c_shards[i])?;
                     }
                 }
                 Ok(())
-
             } else {
                 assert_eq!(in_files.len(), 3);
 
-                let field_elts = in_files.iter().map(|mut f| {
-                    let mut buf = Vec::new();
-                    if f.read_to_end(&mut buf).is_err() {
-                        panic!("Error reading file");
-                    }
-                    F::deserialize_uncompressed(buf.as_slice()).unwrap()
-    
-                }).collect::<Vec<F>>();
-    
-    
+                let field_elts = in_files
+                    .iter()
+                    .map(|mut f| {
+                        let mut buf = Vec::new();
+                        if f.read_to_end(&mut buf).is_err() {
+                            panic!("Error reading file");
+                        }
+                        F::deserialize_uncompressed(buf.as_slice()).unwrap()
+                    })
+                    .collect::<Vec<F>>();
+
                 let sk_shard = field_elts[0];
                 let pwd_shard = field_elts[1];
                 let pwd_guess_shard = field_elts[2];
-    
+
                 // TODO: write them all together
                 let beaver_a = read_beaver_elt(&mut socks, beaver_server).unwrap();
                 let beaver_b = read_beaver_elt(&mut socks, beaver_server).unwrap();
                 let beaver_c = read_beaver_elt(&mut socks, beaver_server).unwrap();
-    
+
                 let rng = &mut ChaCha20Rng::from_entropy();
-    
+
                 let hiding: F = F::rand(rng);
-    
+
                 // ROUND 1: multiplication
                 // Send R - a1, and (PW - PWG) - b1
                 let elts_to_write = (hiding - beaver_a, (pwd_shard - pwd_guess_shard) - beaver_b);
@@ -93,7 +93,7 @@ fn main() -> io::Result<()> {
                         socks[i as usize].write_all(&v1)?;
                     }
                 }
-    
+
                 let mut buf1 = [0u8; F_SIZE * 2];
                 let mut x_sub_a = elts_to_write.0;
                 let mut y_sub_b = elts_to_write.1;
@@ -106,8 +106,8 @@ fn main() -> io::Result<()> {
                         y_sub_b += resp.1;
                     }
                 }
-    
-                // here, 0 is the special node who adds a little extra term, but it doesnt have to be like that. 
+
+                // here, 0 is the special node who adds a little extra term, but it doesnt have to be like that.
                 let z;
                 if rank == 0 {
                     z = beaver_c + x_sub_a * beaver_b + y_sub_b * beaver_a + x_sub_a * y_sub_b;
@@ -116,10 +116,10 @@ fn main() -> io::Result<()> {
                 } else {
                     z = beaver_c + x_sub_a * beaver_b + y_sub_b * beaver_a
                 }
-    
+
                 // ROUND 2: exchange z's
-                // TODO make rounds more generic? it's basically just sending something serializable and deserializing it. 
-    
+                // TODO make rounds more generic? it's basically just sending something serializable and deserializing it.
+
                 let mut v2 = Vec::new();
                 let mut combined_z = z;
                 assert!(z.serialize_uncompressed(&mut v2).is_ok());
@@ -139,17 +139,17 @@ fn main() -> io::Result<()> {
                 }
 
                 let field_to_write: F = sk_shard * (combined_z + F::one());
-    
+
                 let mut result = Vec::new();
                 assert!(field_to_write.serialize_uncompressed(&mut result).is_ok());
-    
+
                 assert_eq!(out_files.len(), 1);
                 let mut out_file = &out_files[0];
                 out_file.write_all(&result)?;
-    
+
                 Ok(())
             }
         }
-        _ => panic!()
+        _ => panic!(),
     }
 }
